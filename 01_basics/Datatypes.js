a795d9a3681c2024b8e5d59054b1b11b29d929bd
Bhagyashree Gore <154083186+229-bhagyashree@users.noreply.github.com>
2024-12-21 16:55:22 +0000

    updated

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all the code as follows the newer version
+//number
+let c=3;
+//bigint
+let n=123456543212345;
+//string
+let s='sanika'
+//boolean(true/false)
+let x=1
+//Null(standalone value)
+let a=null;
+console.log(a);
+console.log(typeof a);
+let b=undefined
+console.log(b);
+console.log(typeof b);
+//Type conversion
+let y='33b'
+let k=Number(y);
+console.log(k);//op as Nan becoz type conversion is not possible
+//1->true,0->false,''->false,'sanika'->true -----this are possible type conversion
+/****operations*****/
+let value=4
+let negValue=-value
+console.log(negValue);
+console.log(4+4);
+console.log(4-4);
+console.log(4*4);
+console.log(4**4);
+//concatenatication of string same as python but "1"+2+2==122 but 2+3+"4"==54
